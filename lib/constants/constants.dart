5333730250bae6a5f5c98805263e47a41fb986f2
Hadi Alsinan <dev.hadialsinan@gmail.com>
2023-03-26 21:16:58 +0300

    New: added animations and fixed smudgging

--- lib/constants/constants.dart
+++ lib/constants/constants.dart
@@ -2,5 +2,5 @@ import 'package:flutter/material.dart';
 
 final BorderRadius innerBorderRadius = BorderRadius.circular(50);
 const BorderRadius outterBorderRadius = BorderRadius.all(Radius.circular(50));
-const double compnentWidth = 350;
+const double compnentWidth = 350.0;
 const double compnentHeight = 100.0;
--- lib/providers/painter_provider.dart
+++ lib/providers/painter_provider.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-final painterStateProvider = StateProvider<List<Offset>>((ref) {
-  return <Offset>[];
-});
+class PainterWidgetProvider {
+  // a provider that will be used to store the points to render the sumdges
+  static final painterStateProvider = StateProvider<List<Offset>>((ref) {
+    return <Offset>[];
+  });
+}
